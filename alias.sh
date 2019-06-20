3d81ea92cbae57fe7e5293552f16f09f44a20232
Jacob Johnston <jacobjohnston@circleci.com>
2019-06-20 18:47:53 -0500

    sleep test

---
+++ alias.sh
@@ -0,0 +1,3 @@
+export CCI_CLOUD_TOKEN=6ae24f9068abc131bafa559f6c6429d02ec05950
+alias cci-validate="circleci config validate --host https://circleci.com --token $CCI_CLOUD_TOKEN"
+alias cci-process="circleci config process --host https://circleci.com --token $CCI_CLOUD_TOKEN"
